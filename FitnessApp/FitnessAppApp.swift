b4b7e5e0ba3e119435b0ac9b3ac58c4327c9635d
Andre Gallegos <andregallegos@ravn.co>
2025-11-08 18:32:01 -0500

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Andre Gallegos on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
